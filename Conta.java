3e70be70818b0b8ae73d4c52af15749682a98872
Pedro Paulo <pedropaulosilvaoliveira2006@gmail.com>
2025-10-30 12:35:52 -0300

    Add files via upload

--- Conta.java
+++ Conta.java
@@ -1,81 +1,83 @@
-package contaBancaria;
+package ContaBancaria;
 
 public class Conta {
-	private String agencia;
-	private int numeroConta;
-	private String cliente;
-	private double saldo;
-	private int senha;
-	private static int ultimaConta = 56120;
-	private double saque;
-	
-	public Conta (String agencia, String cliente, double saldo, int senha, double saque) 
-	{ this.senha = senha;
-	  this.agencia = agencia;
-	  this.numeroConta = ultimaConta;
-	  this.cliente = cliente;
-	  this.saldo = saldo;
-	  this.saque = saque;
-	  
-	  ultimaConta++;
-	 System.out.println("A conta " + numeroConta + " foi criada com sucesso!");
-	}
 
-	public boolean alterarSenha (int senhaAtual,int novaSenha){ 
+		private String agencia;
+		private int numeroConta;
+		private String cliente;
+		private double saldo;
+		private int senha;
+		private static int ultimaConta = 56120;
+		private double saque;
 		
-		boolean confirmaSenha = false;
-		if (senhaAtual == this.senha) 
-	{
-		this.senha = novaSenha;
-		confirmaSenha = true;	
-	}   return confirmaSenha;
-}
- 
-     
-	public boolean sacar (double saque, double novoSaldo)
-	{
-		boolean confirmaSaldo = false;
+		public Conta (String agencia, String cliente, double saldo, int senha, double saque) 
+		{ this.senha = senha;
+		  this.agencia = agencia;
+		  this.numeroConta = ultimaConta;
+		  this.cliente = cliente;
+		  this.saldo = saldo;
+		  this.saque = saque;
+		  
+		  ultimaConta++;
+		 System.out.println("A conta " + numeroConta + " foi criada com sucesso!");
+		}
+
+		public boolean alterarSenha (int senhaAtual,int novaSenha){ 
+			
+			boolean confirmaSenha = false;
+			if (senhaAtual == this.senha) 
+		{
+			this.senha = novaSenha;
+			confirmaSenha = true;	
+		}   return confirmaSenha;
+	}
+	 
+	     
+		public boolean sacar (double saque, double novoSaldo)
+		{
+			boolean confirmaSaldo = false;
+			
+			if (this.saque < this.saldo) 
+		{	novoSaldo = this.saldo - this.saque;
+			saque = novoSaldo;
+			confirmaSaldo = true;
+			System.out.println("Saque efetuado em " + saque + "R$");
+		}   else 
+		{
+			System.out.println("Saldo insuficiente, tente outro valor.");
+			
+		} return confirmaSaldo; 
+	}
+
+
+		public String getAgencia() {
+			return agencia;
+		}
+
+		public int getNumeroConta() {
+			return numeroConta;
+		}
+
+		public String getCliente() {
+			return cliente;
+		}
+
+		public double getSaldo() {
+			return saldo;
+		}
+
+		public int getSenha() {
+			return senha;
+		}
+
+		public static int getUltimaConta() {
+			return ultimaConta;
+		}
+
+
+
+
+
 		
-		if (this.saque < this.saldo) 
-	{	novoSaldo = this.saldo - this.saque;
-		saque = novoSaldo;
-		confirmaSaldo = true;
-		System.out.println("Saque efetuado em " + saque + "R$");
-	}   else 
-	{
-		System.out.println("Saldo insuficiente, tente outro valor.");
-		
-	} return confirmaSaldo; 
-}
-
-
-	public String getAgencia() {
-		return agencia;
 	}
 
-	public int getNumeroConta() {
-		return numeroConta;
-	}
-
-	public String getCliente() {
-		return cliente;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-	public int getSenha() {
-		return senha;
-	}
-
-	public static int getUltimaConta() {
-		return ultimaConta;
-	}
-
-
-
-
-
-	
-}
--- TesteConta.java
+++ TesteConta.java
@@ -1,42 +1,42 @@
-package contaBancaria;
+package ContaBancaria;
 
 public class TesteConta {
 
-	public static void main(String[] args)
-	{
-		Conta cc1 = new Conta("100-0", "Pedro",  100.00,  123, 200.00);
-		Conta cc2 = new Conta ("100-0","Marcos", 100.00,  222, 50.00);
-		Conta cc3 = new Conta ("100-0","Ana Maria", 100.00, 333, 50.00);
+		public static void main(String[] args)
+		{
+			Conta cc1 = new Conta("100-0", "Pedro",  100.00,  123, 200.00);
+			Conta cc2 = new Conta ("100-0","Marcos", 100.00,  222, 50.00);
+			Conta cc3 = new Conta ("100-0","Ana Maria", 100.00, 333, 50.00);
+			
+			String conta;
+			
+			conta = cc1.getAgencia();
+			System.out.println(conta);
+			
+			System.out.println(cc2.getCliente() + " - " + cc2.getAgencia() + " - " + cc2.getNumeroConta());
+			
+			if(cc1.alterarSenha(123, 555)) 
+				{
+					System.out.println("Senha alterada com sucesso");
+				} else {
+					System.out.println("Senha Invalida");
+			}
 		
-		String conta;
-		
-		conta = cc1.getAgencia();
-		System.out.println(conta);
-		
-		System.out.println(cc2.getCliente() + " - " + cc2.getAgencia() + " - " + cc2.getNumeroConta());
-		
-		if(cc1.alterarSenha(123, 555)) 
+			if(cc1.alterarSenha(555, 123)) 
+				{
+					System.out.println("Senha alterada com sucesso");
+				} else 
+				{
+					System.out.println("Senha Invalida");
+				}
+
+			if(cc1.sacar(100, 50)) 
 			{
-				System.out.println("Senha alterada com sucesso");
-			} else {
-				System.out.println("Senha Invalida");
-		}
-	
-		if(cc1.alterarSenha(555, 123)) 
-			{
-				System.out.println("Senha alterada com sucesso");
 			} else 
 			{
-				System.out.println("Senha Invalida");
 			}
-
-		if(cc1.sacar(100, 50)) 
-		{
-		} else 
-		{
 		}
-	}
 }
-		
-		
+			
+			
 
